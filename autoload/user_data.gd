fa9e0225371cac25091e295446d923ef72605f36
zjjchan <justus.chan.2023@scis.smu.edu.sg>
2025-11-03 03:29:58 +0800

    feat: retrieve JWT token from frontend and validate with backend

--- autoload/user_data.gd
+++ autoload/user_data.gd
@@ -2,6 +2,7 @@ extends Node
 
 # Persistent user data storage across scenes
 var user_id: String = ""
+var jwt_token: String = ""
 
 func set_user_id(uid: String) -> void:
 	user_id = uid
@@ -11,3 +12,12 @@ func get_user_id() -> String:
 
 func has_user_id() -> bool:
 	return not user_id.is_empty()
+
+func set_jwt_token(token: String) -> void:
+	jwt_token = token
+
+func get_jwt_token() -> String:
+	return jwt_token
+
+func has_jwt_token() -> bool:
+	return not jwt_token.is_empty()
